67823cda5757618ed3c3076d90d88778cd467f05
mkulik05 <mishakulik2@gmail.com>
2022-12-09 11:56:44 +0300

    fixed bugs

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ clear = lambda: os.system('clear')
 
 def showStr(str):
   print(str)
+  time.sleep(delay)
   clear()
 
 def invert(word):
   return word[::-1]
 
 def strMatch(w1, w2):
-  return w1.toLoweCase() == w2.toLoweCase()
+  return w1.lower() == w2.lower()
 
 words = {5: ["abcde"], 6: ["abcdef"], 7: ["abcdefg"], 8: ["abcdefgh"]}
 
@@ -28,31 +29,32 @@ def selectWord(len):
 
 def round1 ():
   for i in range(5, 9):
-    word = selectWord(i)
-    showStr(word)
-    userWord = input()
     correctAnsws = 0
     while correctAnsws < needCorrectAnsws:
-      if strMatch(word, userWord):
-        print("Correct, {}/{}".format(correctAnsws, needCorrectAnsws))
+      word = selectWord(i)
+      showStr(word)
+      userWord = input()
+      if strMatch(word, invert(userWord)):
+        print("Correct, {}/{}".format(correctAnsws + 1, needCorrectAnsws))
         correctAnsws += 1
       else:
         print("Incorrect")
         correctAnsws = 0
-      
+      clear()
+
 def round2(wordsN):
   for i in range(5, 9):
-    words = []
-    for _ in range(wordsN):
-      words.append(selectWord(i))
-    wordsSet = set(words)
-    showStr(makeQuestion(words))
-    userInp = input()
-    userWords = set([w.toLowerCase() for w in userInp.split()])
     correctAnsws = 0
     while correctAnsws < needCorrectAnsws:
+      words = []
+      for _ in range(wordsN):
+        words.append(selectWord(i))
+      wordsSet = set(words)
+      showStr(makeQuestion(words))
+      userInp = input()
+      userWords = set([w.lower() for w in userInp.split()])
       if userWords == wordsSet:
-        print("Correct, {}/{}".format(correctAnsws, needCorrectAnsws))
+        print("Correct, {}/{}".format(correctAnsws + 1, needCorrectAnsws))
         correctAnsws += 1
       else:
         print("Incorrect")
