6147f2575456e6dc6cb1a4557fcafeb7dd1a20ec
mkulik05 <mishakulik2@gmail.com>
2022-12-09 12:23:39 +0300

    added 3, 4, 5 round

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 
 delay = 5 # in seconds
 needCorrectAnsws = 3
+wordsInRound = 5
+
+words = {5: ["abcde"], 6: ["abcdef"], 7: ["abcdefg"], 8: ["abcdefgh"]}
 
 # clear = lambda: os.system('cls')
 clear = lambda: os.system('clear')
@@ -18,8 +21,6 @@ def invert(word):
 
 def strMatch(w1, w2):
   return w1.lower() == w2.lower()
-
-words = {5: ["abcde"], 6: ["abcdef"], 7: ["abcdefg"], 8: ["abcdefgh"]}
 
 def makeQuestion(arr):
   return ' '.join(arr)
@@ -59,11 +60,69 @@ def round2(wordsN):
       else:
         print("Incorrect")
         correctAnsws = 0
+      clear()
 
+
+def round3(wordsN):
+  for i in range(5, 9):
+    correctAnsws = 0
+    while correctAnsws < needCorrectAnsws:
+      words = []
+      for _ in range(wordsN):
+        words.append(selectWord(i))
+      showStr(makeQuestion(words))
+      userInp = input()
+      userWords = [w.lower() for w in userInp.split()]
+      if userWords == wordsSet:
+        print("Correct, {}/{}".format(correctAnsws + 1, needCorrectAnsws))
+        correctAnsws += 1
+      else:
+        print("Incorrect")
+        correctAnsws = 0
+      clear()
+
+def round4(wordsN):
+  for i in range(5, 9):
+    correctAnsws = 0
+    while correctAnsws < needCorrectAnsws:
+      words = []
+      for _ in range(wordsN):
+        words.append(selectWord(i))
+      showStr(makeQuestion(words))
+      userInp = input()
+      userWords = [invert(w.lower()) for w in userInp.split()]
+      if userWords == wordsSet:
+        print("Correct, {}/{}".format(correctAnsws + 1, needCorrectAnsws))
+        correctAnsws += 1
+      else:
+        print("Incorrect")
+        correctAnsws = 0
+      clear()
+
+def round5(wordsN):
+  for i in range(5, 9):
+    correctAnsws = 0
+    while correctAnsws < needCorrectAnsws:
+      words = []
+      for _ in range(wordsN):
+        words.append(selectWord(i))
+      wordsSet = set(words)
+      showStr(makeQuestion(words))
+      userInp = input()
+      userWords = set([invert(w.lower()) for w in userInp.split()])
+      if userWords == wordsSet:
+        print("Correct, {}/{}".format(correctAnsws + 1, needCorrectAnsws))
+        correctAnsws += 1
+      else:
+        print("Incorrect")
+        correctAnsws = 0
+      clear()
 
 def main ():
   round1()
-  round2(5)
-  print(invert('12as'))
+  round2(wordsInRound)
+  round3(wordsInRound)
+  round4(wordsInRound)
+  round5(wordsInRound)
 
 main()
